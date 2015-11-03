3da13227308f6e41036784150bb61fc541c1dac5
funnythingz <funnythingz69+github@gmail.com>
2015-11-04 00:48:53 +0900

    SplashActivity to MainActivity

--- app/src/main/java/com/funnythingz/memorio/SplashActivity.java
+++ app/src/main/java/com/funnythingz/memorio/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.funnythingz.memorio;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import java.util.Random;
@@ -11,6 +13,8 @@ public class SplashActivity extends AppCompatActivity {
 
     private ImageView mImageView;
 
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +22,32 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         mImageView = (ImageView)findViewById(R.id.logoImageView);
         mImageView.getRootView().setBackgroundColor(randomColor());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startMainActivity();
+                    }
+                }, 3000);
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 
     private int randomColor() {
         Random rnd = new Random();
         return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    private void startMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
