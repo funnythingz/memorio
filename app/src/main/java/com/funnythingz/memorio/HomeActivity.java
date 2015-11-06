07f6ed2ef1412b2767c2f64e0df603be985a8179
funnythingz <funnythingz69+github@gmail.com>
2015-11-07 00:45:04 +0900

    added CreateMemoActivity

--- app/src/main/java/com/funnythingz/memorio/HomeActivity.java
+++ app/src/main/java/com/funnythingz/memorio/HomeActivity.java
@@ -1,8 +1,8 @@
 package com.funnythingz.memorio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,8 +26,7 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startCreateMemoActivity();
             }
         });
     }
@@ -52,5 +51,10 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startCreateMemoActivity() {
+        Intent intent = new Intent(this, CreateMemoActivity.class);
+        startActivity(intent);
     }
 }
