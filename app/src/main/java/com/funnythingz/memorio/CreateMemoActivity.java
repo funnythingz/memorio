b0437be52f31cf7b007977a3020885ddb5065fa5
funnythingz <funnythingz69+github@gmail.com>
2015-11-08 00:16:07 +0900

    keyboard visible

--- app/src/main/java/com/funnythingz/memorio/CreateMemoActivity.java
+++ app/src/main/java/com/funnythingz/memorio/CreateMemoActivity.java
@@ -2,13 +2,19 @@ package com.funnythingz.memorio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.EditText;
 
 public class CreateMemoActivity extends AppCompatActivity {
+
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_create_memo);
+        mEditText = (EditText)findViewById(R.id.editMemo);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
 }
